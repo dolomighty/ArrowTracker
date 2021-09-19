367386494b401a948d8d023a8b5f8db8ea7db413
dolomighty <dolomighty74@gmail.com>
2021-09-19 12:36:50 +0200

    +stili

--- src/Main.cs
+++ src/Main.cs
@@ -13,53 +13,50 @@ namespace ArrowTracker
 {
     
     public class Main : MelonMod
-    {       
+    {
 
 
-//        const int samplerate = 8000;
-//
-//
-//        AudioClip synth_ping( string name="ping", float hz=1000f, float attack_rate=0.1f, float release_rate=0.01f, float release_time=0.1f  ){
-//            // genera una sinusoide con fasi di attack e release
-//
-//            int cliplen = (int)(samplerate * 0.1);
-////            MelonLogger.Msg("samplerate "+samplerate+" cliplen "+cliplen);
-//
-//            AudioClip ac = AudioClip.Create(name, cliplen, 1, samplerate, false );
-//            MelonLogger.Msg(ac.name+" ac.samples "+ac.samples);
-//
-//            float[] data = new float [cliplen];
-////            MelonLogger.Msg("data "+data.Length);
-//
-//            int release_start = (int)(samplerate * release_time);
-//
-//            float gain_tgt = 1;
-//            float rate = attack_rate;
-//
-//            float gain = 0;
-//            int count = 0;
-//            while (count < data.Length)
-//            {
-//                data[count] = (float)Math.Sin(2 * Math.PI * hz * count / samplerate) * gain;
-//                gain += (gain_tgt-gain)*rate;
-//
-//                if(count==release_start){
-//                    gain_tgt = 0;
-//                    rate = release_rate;
-//                }
-//
-//                count++;
-//            }
-//
-//            MelonLogger.Msg(name+" data.Length "+data.Length);
-//            ac.SetData(data,0);
-//            MelonLogger.Msg(name+" ac.samples "+ac.samples);
-//
-//            return ac;
-//        }
-
-
-
+        //        const int samplerate = 8000;
+        //
+        //
+        //        AudioClip synth_ping( string name="ping", float hz=1000f, float attack_rate=0.1f, float release_rate=0.01f, float release_time=0.1f  ){
+        //            // genera una sinusoide con fasi di attack e release
+        //
+        //            int cliplen = (int)(samplerate * 0.1);
+        ////            MelonLogger.Msg("samplerate "+samplerate+" cliplen "+cliplen);
+        //
+        //            AudioClip ac = AudioClip.Create(name, cliplen, 1, samplerate, false );
+        //            MelonLogger.Msg(ac.name+" ac.samples "+ac.samples);
+        //
+        //            float[] data = new float [cliplen];
+        ////            MelonLogger.Msg("data "+data.Length);
+        //
+        //            int release_start = (int)(samplerate * release_time);
+        //
+        //            float gain_tgt = 1;
+        //            float rate = attack_rate;
+        //
+        //            float gain = 0;
+        //            int count = 0;
+        //            while (count < data.Length)
+        //            {
+        //                data[count] = (float)Math.Sin(2 * Math.PI * hz * count / samplerate) * gain;
+        //                gain += (gain_tgt-gain)*rate;
+        //
+        //                if(count==release_start){
+        //                    gain_tgt = 0;
+        //                    rate = release_rate;
+        //                }
+        //
+        //                count++;
+        //            }
+        //
+        //            MelonLogger.Msg(name+" data.Length "+data.Length);
+        //            ac.SetData(data,0);
+        //            MelonLogger.Msg(name+" ac.samples "+ac.samples);
+        //
+        //            return ac;
+        //        }
 
 
 
@@ -81,7 +78,6 @@ namespace ArrowTracker
 //            //float[] asd = new float[1000];
 //            //AudioClip.SetData(ac,asd,asd.Length,0);
 
-            // Settings menu
             ArrowTracker.Settings.OnLoad();
         }
 
@@ -125,22 +121,34 @@ namespace ArrowTracker
 
 
 
+        static string[] dirs_arrows = { "←", "→", "↑", "↓" };
+        static string[] dirs_text = { "left", "right", "front", "back" };
 
-
-
-
-        string facing_from_angle(float angle)
+        string facing_from_angle(float angle,string[] dirs)
         {
-            // angle [0,360]
-            // angle   target facing
+            // angle   target bearing
             // 0       forward
             // >0      right
             // <0      left
-            if (angle >= -135 && angle <=  -45) return "left";
-            if (angle >=  -45 && angle <=  +45) return "front";
-            if (angle >=  +45 && angle <= +135) return "right";
-            return "back";
+            if (angle >= -135 && angle <=  -45) return dirs[0];
+            if (angle >=  -45 && angle <=  +45) return dirs[2];
+            if (angle >=  +45 && angle <= +135) return dirs[1];
+            return dirs[3];
         }
+
+
+
+        string mk_msg(string item, int dist, float angle)
+        {
+            switch (Settings.options.Style)
+            {
+                case 0: return item + " @ " + dist + " meters";                    
+                case 1: return item + " @ " + dist + " meters " + facing_from_angle(angle, dirs_text);                    
+                case 2: return item + " " + facing_from_angle(angle, dirs_arrows) + " " + dist + " meters";                    
+            }
+            return "wtf?!";
+        }
+
 
 
 
@@ -155,7 +163,10 @@ namespace ArrowTracker
             //HUDMessage.AddMessage(msg, 0.5f, true);
             //MelonLogger.Msg(msg);
 
-            if(Settings.options.EnableCSV) log_location(GameManager.m_ActiveScene, pos);
+            //if(Settings.options.EnableCSV) log_location(GameManager.m_ActiveScene, pos);
+
+            // profiling
+            float start = Time.realtimeSinceStartup;
 
             // cerchiamo le frecce
             int layerMask = 0;
@@ -181,6 +192,8 @@ namespace ArrowTracker
                 nearest_xyz = xyz;
             }
 
+            //MelonLogger.Msg("elap "+(Time.realtimeSinceStartup - start));
+
             if (nearest_name == "") return;  // non abbiamo nearest, ciao
 
             // vediamo dove sta il target rispetto al mio facing
@@ -188,11 +201,10 @@ namespace ArrowTracker
             Vector3 tgt = nearest_xyz - pos;
             float dx = Vector3.Dot(tgt, GameManager.GetPlayerTransform().right);
             float dy = Vector3.Dot(tgt, GameManager.GetPlayerTransform().forward);
-            float angle = (float)(Math.Atan2(dx, dy) * 180 / Math.PI);
+            float angle = (float)(Math.Atan2(dx, dy) * 180 / Math.PI);  // -180 <= angle <= +180
 
             //string txt = nearest_name + " dist " + (int)dist + " angle " + (int)(angle) + " facing " + facing_from_angle(angle);
-            string txt = nearest_name + " @ " + (int)dist + " meters";
-            if(Settings.options.EnableDir) txt += " " + facing_from_angle(angle);
+            string txt = mk_msg(nearest_name, (int)dist, angle);
 
             //MelonLogger.Msg(txt);
 
--- src/Settings.cs
+++ src/Settings.cs
@@ -18,13 +18,14 @@ namespace ArrowTracker
         [Description("")]
         public bool EnableMod = true;
 
-        [Name("Enable direction")]
-        [Description("Show left/right/front/back")]
-        public bool EnableDir = true;
+        [Name("Style")]
+        [Description("")]
+        [Choice("GEAR_ARROW @ 3 meters", "GEAR_ARROW @ 3 meters front", "GEAR_ARROW ↑ 3 meters")]
+        public int Style = 1;
 
-        [Name("Enable location logging to disk")]
-        [Description("logs current location to file tracking.csv")]
-        public bool EnableCSV = true;
+        //[Name("Enable location logging to disk")]
+        //[Description("logs current location to file tracking.csv")]
+        //public bool EnableCSV = false;
 
         protected override void OnChange(FieldInfo field, object oldValue, object newValue)
         {           
@@ -40,10 +41,10 @@ namespace ArrowTracker
             //Vars.SettingsBackgroundVignette.GetComponent<UISprite>().mColor = new Color(0f, 0f, 0f, 0f);
         }
 
-        protected override void OnConfirm()
-        {            
-            base.OnConfirm();
-        }
+        //protected override void OnConfirm()
+        //{            
+        //    base.OnConfirm();
+        //}
     }
 
 
@@ -56,51 +57,12 @@ namespace ArrowTracker
     internal static class Settings
     {
         public static SettingsMain options;
-        public static KeyCode returnKeyValue;
-
+        
         public static void OnLoad()
         {
             options = new SettingsMain();
-            ///options.RefreshFields();
             options.AddToModSettings("Arrow Tracker Settings");
         }
-
-
-        public static KeyCode GetInputKeyFromString(int keyStringInt)
-        {
-            switch(keyStringInt)
-            {
-                case 0:  return returnKeyValue = KeyCode.B;
-                case 1:  return returnKeyValue = KeyCode.C;
-                case 2:  return returnKeyValue = KeyCode.F;
-                case 3:  return returnKeyValue = KeyCode.G;
-                case 4:  return returnKeyValue = KeyCode.H;
-                case 5:  return returnKeyValue = KeyCode.I;
-                case 6:  return returnKeyValue = KeyCode.K;
-                case 7:  return returnKeyValue = KeyCode.K;
-                case 8:  return returnKeyValue = KeyCode.L;
-                case 9:  return returnKeyValue = KeyCode.M;
-                case 10: return returnKeyValue = KeyCode.N;
-                case 11: return returnKeyValue = KeyCode.O;
-                case 12: return returnKeyValue = KeyCode.P;
-                case 13: return returnKeyValue = KeyCode.R;
-                case 14: return returnKeyValue = KeyCode.T;
-                case 15: return returnKeyValue = KeyCode.U;
-                case 16: return returnKeyValue = KeyCode.V;
-                case 17: return returnKeyValue = KeyCode.X;
-                case 18: return returnKeyValue = KeyCode.Y;
-                case 19: return returnKeyValue = KeyCode.Z;
-                case 20: return returnKeyValue = KeyCode.Insert;
-                case 21: return returnKeyValue = KeyCode.Home;
-                case 22: return returnKeyValue = KeyCode.End;
-                case 23: return returnKeyValue = KeyCode.PageUp;
-                case 24: return returnKeyValue = KeyCode.PageDown;
-                case 25: return returnKeyValue = KeyCode.Pause;
-                case 26: return returnKeyValue = KeyCode.Clear;
-            }
-            return returnKeyValue;
-        }
-
     }
 }
 
